32420f1b49a929a3c134190af68270c6a4852d5c
Neves-P <p.m.santos.neves@rug.nl>
2020-09-24 20:53:57 +0200

    Add run_pipeline stub

---
+++ R/run_pipeline.R
@@ -0,0 +1,16 @@
+run_pipeline <- function(experiment_name,
+                         n_replicates = 1000,
+                         seed_start = 1) {
+
+  # extract_parameters
+
+  # run_iw_sims
+
+  # extract_first_lineage
+
+  # convert_to_phylo
+
+  # save_file
+
+
+}
