baad044a954f2c5ac26c41f33ce4aae74269e1f3
Neves-P <p.m.santos.neves@rug.nl>
2020-09-24 15:22:08 +0200

    Seed handling in run_iw_sims

--- R/run_iw_sims.R
+++ R/run_iw_sims.R
@@ -3,18 +3,45 @@
 #' @inheritParams default_params_doc
 #'
 #' @return DAISIE object containing IW simulations.
-#' @export
+#'
+#'@examples
+#' out <- phylometricsims:::run_iw_sims(
+#'   totaltime = 4,
+#'   sim_pars = c(1, 2, 3, 4, 5),
+#'   n_mainland_spec = 1000,
+#'   n_replicates = 2,
+#'   seed_start = 1
+#' )
 #'
 #' @author Pedro Neves
-run_iw_sims <- function(totaltime, sim_pars, n_mainland_spec, replicates) {
-  sim_output <- DAISIE::DAISIE_sim_constant_rate(
-    time = totaltime,
-    M = n_mainland_spec,
-    pars = sim_pars,
-    replicates = replicates,
-    divdepmodel = "IW",
-    plot_sims = FALSE,
-    verbose = FALSE
-  )
+run_iw_sims <- function(totaltime,
+                        sim_pars,
+                        n_mainland_spec,
+                        n_replicates,
+                        seed_start) {
+
+  sim_output <- list()
+  seeds <- seed_start:n_replicates
+
+  for (i in seq_len(n_replicates)) {
+
+    set.seed(
+      seeds[i],
+      kind = "Mersenne-Twister",
+      normal.kind = "Inversion",
+      sample.kind = "Rejection"
+    )
+
+    sim_output[[i]] <- DAISIE::DAISIE_sim_constant_rate(
+      time = totaltime,
+      M = n_mainland_spec,
+      pars = sim_pars,
+      replicates = 1,
+      divdepmodel = "IW",
+      plot_sims = FALSE,
+      verbose = FALSE
+    )
+  }
+
   return(sim_output)
 }
