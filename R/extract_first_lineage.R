80f43a5138898fc9b02c37b5c86cee9359170526
Neves-P <p.m.santos.neves@rug.nl>
2020-09-24 15:21:25 +0200

    Add extract_first_lineage

---
+++ R/extract_first_lineage.R
@@ -0,0 +1,29 @@
+#' Extract first lineagebranching times vector
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return List with a level per replicate, each containing a numeric vector
+#'   with branching times of first lineage of the simulation.
+#' @export
+#'
+#' @examples
+#' out <- phylometricsims:::run_iw_sims(
+#'   totaltime = 4,
+#'   sim_pars = c(1, 2, 3, 4, 5),
+#'   n_mainland_spec = 1000,
+#'   n_replicates = 2,
+#'   seed_start = 1
+#' )
+#'
+#' brts_list <- phylometricsims:::extract_first_lineage_brts(out)
+#'
+#' @author Pedro Neves
+extract_first_lineage_brts <- function(iw_sims) {
+
+  first_lineage_brts <- list()
+
+  for (i in seq_along(iw_sims)) {
+    first_lineage_brts[[i]] <- iw_sims[[i]][[1]][[2]]$branching_times
+  }
+  first_lineage_brts
+}
