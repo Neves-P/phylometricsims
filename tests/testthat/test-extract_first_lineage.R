e7340dc0b1071498ca8d8e906614ff019e330799
Neves-P <p.m.santos.neves@rug.nl>
2020-09-24 15:37:33 +0200

    test extract_first_lineage

---
+++ tests/testthat/test-extract_first_lineage.R
@@ -0,0 +1,25 @@
+test_that("use", {
+
+  totaltime <- 4
+  sim_pars <- c(1, 2, 3, 4, 5)
+  n_mainland_spec <- 1000
+  n_replicates <- 2
+  seed_start <- 1
+
+  out <- phylometricsims:::run_iw_sims(
+    totaltime = totaltime,
+    sim_pars = sim_pars,
+    n_mainland_spec = n_mainland_spec,
+    n_replicates = n_replicates,
+    seed_start = seed_start
+  )
+
+  brts_list <- phylometricsims:::extract_first_lineage_brts(out)
+
+
+  expect_length(brts_list, n_replicates)
+  expect_equal(brts_list[[1]], c(1, 0.924481816687166))
+  expect_equal(brts_list[[2]], c(1, 0.81346475594585899))
+
+
+})
