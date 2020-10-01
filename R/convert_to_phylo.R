c042c102e04dff231a72b6642681c88a49743d7d
Neves-P <p.m.santos.neves@rug.nl>
2020-10-02 00:20:47 +0200

    Fix and document convert_to_phylo()

--- R/convert_to_phylo.R
+++ R/convert_to_phylo.R
@@ -1,7 +1,12 @@
+#' Converts list of branching time vectors to phylo objects
+#'
+#' @inheritParams default_params_doc
+#'
+#' @return List with phylo objects.
 convert_to_phylo <- function(brts) {
 
     tree <- lapply(
-      X = brts[[1]],
+      X = brts,
       FUN = DDD::brts2phylo,
       root = FALSE,
       tip.label = NULL
