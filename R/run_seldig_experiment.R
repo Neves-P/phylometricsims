0bb954cf3e19df32fa30ace1e0b057fb7b5174b5
Neves-P <p.m.santos.neves@rug.nl>
2020-10-05 19:24:45 +0200

    better example

--- R/run_seldig_experiment.R
+++ R/run_seldig_experiment.R
@@ -21,7 +21,9 @@
 #' @author Pedro Neves
 #'
 #' @examples
-#' run_seldig_experiment(n_param_sets_to_run = 2)
+#' \dontrun{
+#' run_seldig_experiment(n_param_sets_to_run = 2000)
+#' }
 run_seldig_experiment <- function(n_param_sets_to_run,
                                   n_replicates = 1) {
 
