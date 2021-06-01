7a629a08752b7dc8b0727073e416b53d86d78e13
Neves-P <p.m.santos.neves@rug.nl>
2021-06-01 21:51:36 +0200

    Sleep less

--- R/run_pipeline.R
+++ R/run_pipeline.R
@@ -91,7 +91,7 @@ run_pipeline <- function(n_param_sets_to_run,
     output <- list(iw_sims = iw_sims, iw_brts = iw_brts, phylo = phylo)
 
     if (save_to_file && length(phylo$tip.label) >= 20) {
-      Sys.sleep(1)
+      Sys.sleep(0.1)
       output_name <- paste0("ve_", param_set, ".rds")
       if (!dir.exists("output")) dir.create("output")
       output_path <- file.path("output", output_name)
