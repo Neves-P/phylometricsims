a6b10af53ffc1706c35870a0a18f7019bc6ecf2d
Neves-P <p.m.santos.neves@rug.nl>
2021-06-01 19:47:48 +0200

    Print

--- R/run_pipeline.R
+++ R/run_pipeline.R
@@ -43,6 +43,7 @@ run_pipeline <- function(param_set,
   }
 
   iw_brts <- extract_first_lineage_brts(iw_sims = iw_sims)
+  print(iw_brts)
   phylo <- DDD::brts2phylo(times = iw_brts, root = FALSE, tip.label = NULL)
 
   output <- list(iw_sims = iw_sims, iw_brts = iw_brts, phylo = phylo)
